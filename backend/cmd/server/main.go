6d2355f0ac22bc143b73517e1e38585d946ca5ef
Alberto <albertojunior848423803@gmail.com>
2025-03-07 21:32:38 +0300

    changes

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testee")
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Database ping failed:", err)
+	}
+}
